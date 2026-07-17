e2b1df05d2d2b2b5e1dbfa601d292c469d24b146
Serein <vxndez7@gmail.com>
2026-07-16 19:02:09 -0500

    Correción de errores

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Reemplaza esta URL por la que te dé Railway al terminar el paso 2 de la guía de deploy.
   apiUrl: 'https://sistema-epp-final-production.up.railway.app',
 };
